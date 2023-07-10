ccae83f31ed8c1b1b7e9bfb5ce5a7679b00795e8
Bhagyarajuv <bhagya.raju@myn.global>
2023-07-10 23:54:24 +0530

    context done with this

---
+++ PeekPop/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  PeekPop
+//
+//  Created by Chu Hung on 12/04/2023.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
